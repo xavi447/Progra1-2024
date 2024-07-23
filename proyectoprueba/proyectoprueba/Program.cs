5a0fa2f118acf6f25957ce7813b6fad1897871d8
xavi447 <alexandersoriano1022@gmail.com>
2024-07-23 08:07:40 -0600

    prioridad de operadores aritmeticos

--- proyectoprueba/proyectoprueba/Program.cs
+++ proyectoprueba/proyectoprueba/Program.cs
@@ -10,6 +10,11 @@ namespace proyectoprueba
     {
         static void Main(string[] args)
         {
+            //prioridad de los operadores aritmeticos
+            int respuesta = 5 + 8 - 2 * 5 / 4 + 1;
+            Console.WriteLine("la respuesta correcta es: {0}", respuesta);
+            //pause
+            Console.ReadLine();
         }
     }
 }
