aef08b17259bcde2f20dce2f8957244174afce68
xavi447 <alexandersoriano1022@gmail.com>
2024-07-23 08:44:46 -0600

    notas de computos

--- proyectoprueba/proyectoprueba/Program.cs
+++ proyectoprueba/proyectoprueba/Program.cs
@@ -11,8 +11,32 @@ namespace proyectoprueba
         static void Main(string[] args)
         {
             //prioridad de los operadores aritmeticos
-            int respuesta = 5 + 8 - 2 * 5 / 4 + 1;
-            Console.WriteLine("la respuesta correcta es: {0}", respuesta);
+            // obtener la nota final de la materia de prograI
+            Console.Write("lab1: ");
+            double lab1 = double.Parse(Console.ReadLine());
+
+            Console.Write("lab2: ");
+            double lab2 = double.Parse(Console.ReadLine());
+
+            Console.Write("parcial1: ");
+            double parcial1 = double.Parse(Console.ReadLine());
+
+            double c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
+            Console.WriteLine("la nota de c1 es: {0}", c1);
+
+            //computo2
+            Console.Write("lab1: ");
+            double lab13 = double.Parse(Console.ReadLine());
+
+            Console.Write("lab2: ");
+            double lab4 = double.Parse(Console.ReadLine());
+
+            Console.Write("parcial2: ");
+            double parcial2 = double.Parse(Console.ReadLine());
+
+            double c2 = lab13 * 30 / 100 + lab4 * 30 / 100 + parcial2 * 40 / 100;
+            Console.WriteLine("la nota de c2 es: {0}", c2);
+            // tarea de nota c3 y nota final.
             //pause
             Console.ReadLine();
         }
