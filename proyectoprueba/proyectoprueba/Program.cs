a8a9f500b9138b1fc2fe57f7251254bef21b23b0
xavi447 <alexandersoriano1022@gmail.com>
2024-07-24 09:49:24 -0600

    determinar el promedio de una serie de numeros

--- proyectoprueba/proyectoprueba/Program.cs
+++ proyectoprueba/proyectoprueba/Program.cs
@@ -10,53 +10,18 @@ namespace proyectoprueba
     {
         static void Main(string[] args)
         {
-            //prioridad de los operadores aritmeticos
-            // obtener la nota final de la materia de prograI
-            Console.WriteLine("ingrese las notas del computo 1");
-            Console.Write("lab1: ");
-            double lab1 = double.Parse(Console.ReadLine());
+            //prioridad de los operadores basicos 
+            //ejercicio de obtener una serie de numeros 
+            int[] serie = new int[] { 5, 4, 6, 8, 9, 10 };
+            int suma = 0;
+            foreach (int num in serie)
+            {
+                suma = suma + num;
+            }
+            double prom = suma / serie.Length;
+            Console.WriteLine("la suma es: {0}, el promedio: {1}", suma, prom);
 
-            Console.Write("lab2: ");
-            double lab2 = double.Parse(Console.ReadLine());
-
-            Console.Write("parcial1: ");
-            double parcial1 = double.Parse(Console.ReadLine());
-
-            double c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
-            Console.WriteLine("la nota de c1 es: {0}", c1);
-
-            //computo2
-            Console.WriteLine("ingrese las notas del computo 2");
-            Console.Write("lab1: ");
-            double lab13 = double.Parse(Console.ReadLine());
-
-            Console.Write("lab2: ");
-            double lab4 = double.Parse(Console.ReadLine());
-
-            Console.Write("parcial2: ");
-            double parcial2 = double.Parse(Console.ReadLine());
-
-            double c2 = lab13 * 30 / 100 + lab4 * 30 / 100 + parcial2 * 40 / 100;
-            Console.WriteLine("la nota de c2 es: {0}", c2);
-
-            //computo 3
-            Console.WriteLine("ingrese las notas del computo 3");
-            Console.Write("lab1: ");
-            double lab5 = double.Parse(Console.ReadLine());
-
-            Console.Write("lab2: ");
-            double lab6 = double.Parse(Console.ReadLine());
-
-            Console.Write("parcial3: ");
-            double parcial3 = double.Parse(Console.ReadLine());
-
-            double c3 = lab5 * 30 / 100 + lab6 * 30 / 100 + parcial3 * 40 / 100;
-            Console.WriteLine("la nota de c3 es: {0}", c3);
-            // nota final
-            double NF = c1 + c2 + c3 ;
-            Console.WriteLine("la nota final de la materia es:{0} ", NF/3);
-            // tarea de nota c3 y nota final.
-            //pause
+            //pausa
             Console.ReadLine();
         }
     }
