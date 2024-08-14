12f4db0b12380b1e7b2c7034ee25c579e2f1835d
xavi447 <alexandersoriano1022@gmail.com>
2024-08-14 09:59:55 -0600

    secencia de fobinacci

---
+++ prograi/prograi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prograi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //obtener la secuencia de fibonacci
+            int a = 0, b = 1, c = 1;
+            while (c < 100)
+            {
+                Console.WriteLine("{0}+{1}={2}", a, b, c);
+                b = a;
+                a = c;
+                c = a + b;
+            }
+            Console.ReadLine();
+        }
+    }
+}
