0d0a6f03b677a11aabcf7ec7535fe2b0b2e96de5
xavi447 <alexandersoriano1022@gmail.com>
2024-08-13 20:37:06 -0600

    calculo de sueldo neto y pretaciones de ley

--- proyectoprueba/proyectoprueba/Program.cs
+++ proyectoprueba/proyectoprueba/Program.cs
@@ -10,20 +10,36 @@ namespace proyectoprueba
     {
         static void Main(string[] args)
         {
-            //uso de matrices 
-            int[,] matriz = new int[,] {
+            //uso de matrices. calcular el sueldo de una persona 
 
-                {2,8,6,7,5 },
-                {7,9,1,6,3 }
-            };
-            for (int i = 0; i < 2; i++)
+            double[,] matriz = new double[,]
             {
-                for (int j = 0; j < 5; j++)
-                    Console.Write("{0} ", matriz[i, j]);
+                { 0.01, 487.6, 0, 0 },
+                { 487.61, 642.85, 10,17.48},
+                { 642.86, 915.81, 10,32.70 },
+                { 915.82, 2058.67, 20, 60.00},
+                { 2058.68, 9999999, 30, 288.57 },
+            };
+            Console.Write("Sueldo: ");
+            double sueldo = double.Parse(Console.ReadLine()),
+                afp = sueldo * 6.25 / 100,
+                isss = sueldo * 3 / 100;
+            isss = sueldo * 3 / 100;
+               
+            sueldo -= afp;
+            double isr = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (sueldo >= matriz[i, 0] && sueldo <= matriz[i, 1])
                 {
-                    Console.WriteLine("\n");
+                    isr = (sueldo - (matriz[i, 0] - 0.01)) * matriz[i, 2] / 100 + matriz[i, 3];
                 }
             }
+            sueldo -= isss;
+            sueldo -= isr;
+            Console.WriteLine("Sueldo Neto: {0}, AFP: {1}, ISSS: {2}, ISR: {3}", Math.Round(sueldo, 2), afp, isss, Math.Round(isr, 2));
+            Console.ReadLine();
+
             // pausa 
             Console.ReadKey();
         }
