879f659bc92ad405fa218944d7c3be86dd76ef92
xavi447 <alexandersoriano1022@gmail.com>
2024-08-20 16:22:41 -0600

    avance de la tarea de progra

--- prograi/prograi/Program.cs
+++ prograi/prograi/Program.cs
@@ -9,44 +9,156 @@ namespace prograi
     class Program
     {
         static void Main(string[] args)
-            //uso de funciones
+        //uso de funciones
         {
-            Console.Write("Ingrese la serie de numeros separados por comas: ");
-            String serie = Console.ReadLine();
-            String [] numeros = serie.Split(',');
-            
-            Console.WriteLine("La media aritmetica es: {0}, y la desviacion tipica es: {1}; y la medida armonica es: {2}", media(numeros), tipica(numeros), armonica(numeros));
+            string continuar = "s";
+            while (continuar == "s")
+            {
+                Console.WriteLine("***Menu de opciones***");
+                Console.WriteLine("opcion 1: Conversion de monedas");
+                Console.WriteLine("Opcion 2: Conversion de masas");
+                Console.WriteLine("Opcion 3: conversion de Volumen");
+                Console.WriteLine("Opcion 4: Conversion de Longitud");
+                Console.WriteLine("Opcion 5: Conversion de almacenamiento");
+                Console.WriteLine("Opcion 6: conversion de tiempo");
+                Console.WriteLine("Opcion 7: salir");
+                Console.Write("opcion: ");
+                int opcion = int.Parse(Console.ReadLine());
+                Console.Clear();
+                switch (opcion)
+                {
+                    case 1:
+                        Cmonedas();
+                        break;
 
-            Console.ReadLine();
+                    case 2:
+
+                        break;
+
+                    case 3:
+
+                        break;
+
+                    case 4:
+                        Lconvertir();
+                        break;
+
+                    case 5:
+                        break;
+
+                    case 6:
+                        break;
+
+                    case 7:
+                        break;
+
+                }
+
+
+
+
+
+
+
+
+
+
+                Console.ReadLine();
+
+
+
+
+
+            }
         }
-        static double media(string[] serie)
+        static void Cmonedas()
         {
-            double media = 0;
-            foreach (string num in serie)
+            string[] monedas = { "Dólar", "Euro", "Peso MX", "Peso col", "Córdobas", "Quetzal", "Lempira", "Yenes", "Libras Esterlinas", "Pesos Arg" };
+            double[] tasasMoneda = { 1, 0.85, 17.20, 4174.89, 36.42, 7.72, 24.67, 148.65, 0.76, 365.20 };
+
+            while (true)
             {
-                media += int.Parse(num);
+                
+                Console.WriteLine("escoge la moneda que quieres convertir:");
+                for (int i = 0; i < monedas.Length; i++)
+                    Console.WriteLine($"{i + 1}. {monedas[i]}");
+                int maconvertir = int.Parse(Console.ReadLine() ?? "0") - 1;
+                if (maconvertir < 0 || maconvertir >= monedas.Length)
+                {
+                    Console.WriteLine("Opción no válida.");
+                    continue;
+                }
+
+                
+                Console.WriteLine("Elegiste la opción {0}. a que moneda lo quieres convertir", maconvertir + 1);
+                for (int i = 0; i < monedas.Length; i++)
+                    Console.WriteLine($"{i + 1}. {monedas[i]}");
+                int mconvertida = int.Parse(Console.ReadLine() ?? "0") - 1;
+                if (mconvertida < 0 || mconvertida >= monedas.Length)
+                {
+                    Console.WriteLine("Opción no válida.");
+                    continue;
+                }
+
+                
+                Console.Write("que cantidad deseas convertir: ");
+                double cantidad = double.Parse(Console.ReadLine() ?? "0");
+
+                
+                double resultado = cantidad * (tasasMoneda[mconvertida] / tasasMoneda[maconvertir]);
+                Console.WriteLine($"{cantidad} {monedas[maconvertir]} = {resultado} {monedas[mconvertida]}");
+
+
+
             }
-            return media / serie.Length;
         }
-        static double tipica(string[] serie)
+        static void Lconvertir()
         {
-            double tipica = 0,
-                     m = media(serie);
-            foreach (string num in serie)
-            {
-                tipica += Math.Pow(int.Parse(num) - m, 2);
-            }
-            tipica = Math.Sqrt(tipica / serie.Length);
-            return Math.Round(tipica);
-        }
-        static double armonica (string [] serie) {
-            double armonica = 0;
-            foreach (string num in serie)
-            {
-                armonica += 1 / int.Parse(num);
-            }
-            armonica = serie.Length / armonica;
-            return Math.Round(armonica);
+
+            
+            string[] unidad =
+            {"Metro", "Kilometro", "Hectometro", "Decametro", "Decimetro", "Centimetro", "Milimetro", "Pies", "Pulgadas", "Yardas" };
+                 double[] longitud =
+                {1, 1000, 100, 10, 0.1, 0.01, 0.001, 0.3048, 0.254, 0.9144  };
+
+           
+                while (true)
+                {
+                    
+                    Console.WriteLine("Seleccione la unidad que desea convertir:");
+                    for (int i = 0; i < unidad.Length; i++)
+                        Console.WriteLine($"{i + 1}. {unidad[i]}");
+                    int Uaconvertir = int.Parse(Console.ReadLine() ?? "0") - 1;
+                    if (Uaconvertir < 0 || Uaconvertir >= unidad.Length)
+                    {
+                        Console.WriteLine("Opción no válida.");
+                        continue;
+                    }
+
+                    
+                    Console.WriteLine("Elegiste la opción {0}. ahora elige la unidad a la que lo quieres convertir:", Uaconvertir + 1);
+                    for (int i = 0; i < unidad.Length; i++)
+                        Console.WriteLine($"{i + 1}. {unidad[i]}");
+                    int Uaconvertida = int.Parse(Console.ReadLine() ?? "0") - 1;
+                    if (Uaconvertida < 0 || Uaconvertida >= unidad.Length)
+                    {
+                        Console.WriteLine("Opción no válida.");
+                        continue;
+                    }
+
+              
+                    Console.Write("escribe la unidad que deseas convertir: ");
+                    double Lconvertir = double.Parse(Console.ReadLine() ?? "0");
+
+                    
+                    double resultado = Lconvertir * (longitud[Uaconvertir] / longitud[Uaconvertida]);
+                    Console.WriteLine($"{Lconvertir} {unidad[Uaconvertir]} = {resultado} {unidad[Uaconvertida]}");
+
+                  
+                }
+            
+
+           
         }
     }
 }
